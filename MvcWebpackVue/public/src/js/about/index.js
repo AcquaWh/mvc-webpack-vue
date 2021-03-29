6cab66243849b9402a94f67f1f8089c778338b76
Fernanda <acquaentertainment@gmail.com>
2021-03-29 01:54:52 -0700

    Initial commit

---
+++ MvcWebpackVue/public/src/js/about/index.js
@@ -0,0 +1,18 @@
+﻿import Vue from "vue";
+import { mapState } from "vuex";
+import store from "../store";
+import Vuetable from 'vuetable-2';
+
+const vm = new Vue({
+    el: "#app-about",
+    store,
+    components: {
+        Vuetable
+    },
+    data() {
+        return {};
+    },
+    computed: {
+        ...mapState(["anotherMessage"])
+    }
+});
